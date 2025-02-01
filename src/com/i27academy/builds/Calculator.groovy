f41bbe7bf1edf1039a5abc46fcd018adf8f71747
ram <reddylavanuru315@gmail.com>
2025-02-01 11:40:30 +0530

    all ommited

---
+++ src/com/i27academy/builds/Calculator.groovy
@@ -0,0 +1,19 @@
+package com.i27academy.builds
+
+class Calculator {
+    def jenkins
+    Calculator(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    def add(firstNumber, secondNumber) {
+    return firstNumber+secondNumber
+    }
+
+
+    def multiply (firstNumber, secondNumber) {
+    return firstNumber*secondNumber
+    }
+}
+
+
---
+++ vars/firstPipeline.groovy
@@ -0,0 +1,24 @@
+import com.i27academy.builds.Calculator
+
+def call(Map pipelineparams) {
+    Calculator calculator = new Calculator(this)
+
+    pipeline {
+        agent {
+            label 'k8s-slave'
+        }
+        environment {
+            APPLICATION_NAME = "${pipelineparams.appName}"
+        }
+        stages {
+            stage('addition') {
+                steps {
+                    script {
+                        echo "printing of sum of 2 numbers"
+                        println calculator.add(3,4)
+                        echo "microservice name is ${APPLICATION_NAME}"
+                    }
+                }
+            }
+        }
+    }
