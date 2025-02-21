a8896894c6762b064a38614c99981f55d95e711b
ram <reddylavanuru315@gmail.com>
2025-02-21 10:48:39 +0530

    cleaning workspace

--- vars/helmPipeline.groovy
+++ vars/helmPipeline.groovy
@@ -231,6 +231,12 @@ def call(Map pipelineParams) {
                         }
                         
                     }
+                    stage('clean_workspace') {
+                        steps {
+                            echo **cleaning workspace**
+                            cleanWs()
+                        }
+                    }
                     
         }
     }
